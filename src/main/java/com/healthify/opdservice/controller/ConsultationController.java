75d7c005baab9bb6d5029c61cc2e7b563ccebe42
Tejaswini Turlapati <tejteja097@gmail.com>
2026-02-11 09:41:48 +0530

    ferch header

--- src/main/java/com/healthify/opdservice/controller/ConsultationController.java
+++ src/main/java/com/healthify/opdservice/controller/ConsultationController.java
@@ -2,12 +2,15 @@ package com.healthify.opdservice.controller;
 
 import com.healthify.opdservice.DTO.request.CreateConsultationRequest;
 import com.healthify.opdservice.func.CreateConsultationFunc;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
 @RestController
 @RequestMapping("/consultation")
@@ -19,6 +22,10 @@ public class ConsultationController {
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ResponseEntity<String> createConsultation(@Valid @RequestBody  CreateConsultationRequest request){
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest requestSvlt = requestAttributes.getRequest();
+
+        logger.info("*******custom Header************ {}", requestSvlt.getHeader("custom"));
         logger.info("in ConsultationController.createConsultation");
         String result = createConsultationFunc.createConsultation(request);
         return  ResponseEntity.ok(result);
