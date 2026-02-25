43dc7fdb4fd08c17bc1513827f527a9112c7319e
Tejaswini Turlapati <tejteja097@gmail.com>
2026-02-25 10:12:17 +0530

    integration tests

--- src/test/java/com/healthify/opdservice/integrationTests/controller/CreateConsultationFullIntegrationTest.java
+++ src/test/java/com/healthify/opdservice/integrationTests/controller/CreateConsultationFullIntegrationTest.java
@@ -1,33 +1,25 @@
 package com.healthify.opdservice.integrationTests.controller;
 
 
-import com.healthify.opdservice.controller.ConsultationController;
-import com.healthify.opdservice.func.CreateConsultationFunc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ConsultationController.class)
-public class CreateConsultationIntegrationTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CreateConsultationFullIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    @MockitoBean
-    CreateConsultationFunc createConsultationFunc;
-
     @Test
-    public void testConsultationCreation_success() throws Exception {
-
+    public void test_creatConsultation_success() throws Exception {
         String request = """
                 {
                     "firstConsultation": true,
@@ -40,27 +32,25 @@ public class CreateConsultationIntegrationTest {
                   }
                 """;
 
-        when(createConsultationFunc.createConsultation(any())).thenReturn("123");
-
         mockMvc.perform(post("/consultation/create").contentType("application/json").content(request)).andExpect(status().isOk());
     }
 
-
     @Test
-    public void testConsultationCreation_validation_failure() throws Exception{
-    String request = """
-            {
+    public void test_createConsultation_invalid_docName() throws Exception {
+        String request = """
+                {
+                    "firstConsultation": true,
                     "parentConsultationId": "CONS-12345",
-                    "symptoms": "Fever",
+                    "symptoms": "Fever , cough, since two days",
                     "paymentId": ["PAY-101", "PAY-102"],
                     "time": "2026-02-05T10:30:00",
-                    "doctorName": "Ram",
+                    "doctorName": "xyz",
                     "patientName": "Neha Singh"
                   }
-            """;
+                """;
 
-    mockMvc.perform(post("/consultation/create").content(request).contentType("application/json")).andExpect(status().is4xxClientError());
+        mockMvc.perform(post("/consultation/create").content(request).contentType("application/json")).andExpect(status().is5xxServerError()).andExpect(content().string("{\"errorCode\":\"DOCTOR_NOT_FOUND\",\"errorMessage\":\"no such doctor found with name xyz\",\"statusCode\":500,\"timeStamp\":null}"));
+    }
 
-}
 
 }
