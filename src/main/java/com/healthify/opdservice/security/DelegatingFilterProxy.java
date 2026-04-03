5a2c3e47ab8eb34b75d09d4a0fa28e1e7dc7369d
Tejaswini Turlapati <tejteja097@gmail.com>
2026-04-03 10:25:32 +0530

    add spring security

--- src/main/java/com/healthify/opdservice/security/DelegatingFilterProxy.java
+++
@@ -1,4 +0,0 @@
-package com.healthify.opdservice.security;
-
-public class DelegatingFilterProxy {
-}
---
+++ src/main/java/com/healthify/opdservice/security/config/DelegatingFilterProxy.java
@@ -0,0 +1,6 @@
+package com.healthify.opdservice.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class DelegatingFilterProxy extends AbstractSecurityWebApplicationInitializer {
+}
